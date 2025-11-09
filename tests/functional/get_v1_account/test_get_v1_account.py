0cfb2aad7557435dc14225fae81a897917fb5f46
Иван Пономарев <panama.piv@yandex.ru>
2025-11-09 21:23:24 +0200

    Вынес проверку полей метода get_v1_account в отдельный чекер

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,11 +1,4 @@
-from hamcrest import (
-    all_of,
-    assert_that,
-    has_items,
-    has_properties,
-    has_property,
-    starts_with,
-)
+from checkers.get_v1_account import GetV1Account
 
 
 def test_get_v1_account(auth_account_helper):
@@ -14,60 +7,4 @@ def test_get_v1_account(auth_account_helper):
     response = auth_account_helper.get_current_user()
 
     # Проверяем поля тела ответа
-    assert_that(
-        response,
-        all_of(
-            has_property(
-                "resource",
-                has_property(
-                    "login",
-                    starts_with("iponomarev"),
-                ),
-            ),
-            has_property(
-                "resource",
-                has_property(
-                    "settings",
-                    has_property("color_schema", "Modern"),
-                ),
-            ),
-            has_property(
-                "resource",
-                has_property(
-                    "settings",
-                    has_property(
-                        "paging",
-                        has_properties(
-                            {
-                                "posts_per_page": 10,
-                                "comments_per_page": 10,
-                                "topics_per_page": 10,
-                                "messages_per_page": 10,
-                                "entities_per_page": 10,
-                            },
-                        ),
-                    ),
-                ),
-            ),
-            has_property(
-                "resource",
-                has_property(
-                    "roles",
-                    has_items("Guest", "Player"),
-                ),
-            ),
-            has_property(
-                "resource",
-                has_property(
-                    "rating",
-                    has_properties(
-                        {
-                            "enabled": True,
-                            "quality": 0,
-                            "quantity": 0,
-                        },
-                    ),
-                ),
-            ),
-        ),
-    )
+    GetV1Account.check_response_values(response)
